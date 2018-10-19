3f3d3c99d1c31703a202a4086760ccef3d6432b5
ZeroByte.ID Warehouse <38853171+zerobyte-id@users.noreply.github.com>
2018-10-19 11:49:48 +0700

    Update cookieplay-50.php

--- web/cookieplay-50.php
+++ web/cookieplay-50.php
@@ -1,9 +1,10 @@
 <?php
 	$flag = "FLAG{Random_String}";
+
 	setcookie("role", "user");
 	if($_COOKIE["role"] == "admin") {
-    	echo "Welcome back admin! <br>".PHP_EOL;
-    	echo "$flag".PHP_EOL;
+    		echo "Welcome back admin! <br>".PHP_EOL;
+    		echo "$flag".PHP_EOL;
 	}
 	else {
 		echo "Hello user!";

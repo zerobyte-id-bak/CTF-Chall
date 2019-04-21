4b19c285457844ce216dce9cd2d6db696bf74594
ZeroByte.ID Warehouse <38853171+zerobyte-id@users.noreply.github.com>
2019-04-22 02:55:39 +0700

    Create BasicMath-75.c

---
+++ reverse-engineering/BasicMath-75.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/*
+* BasicMath-75.c
+* - pa = 18992
+* - ss = 12345
+* => pa + ss = 31337
+* 
+* Compile:
+* # gcc BasicMath-75.c -o basicmath
+*/
+
+int main()
+{
+	int pa = 18992;
+	int ss = 12345;
+	int pwd;
+	printf("Enter the Key : ");
+	scanf("%d",&pwd);
+	if (pa + ss == pwd)
+	{
+		int i;
+		char str[100] = "iodjwhutxdfnmhurelwfwi";
+		// iodjwhutxdfnmhurelwfwi = flagterquackjerobitctf (Encrypted with Caesar Cipher +3)
+
+			for(i = 0; (i < 100 && str[i] != '\0'); i++)
+			{
+				str[i] = str[i] - 3;
+			}
+			printf("FLAG: %s\n", str);
+	}
+	else {
+		printf("FALSE!");
+	}
+	return 0;
+}
